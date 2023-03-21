8c97f181ce8c08c439646c524c6ccaa7cf9c39d2
Jaime Díez <jaimecuentacod@gmail.com>
2023-03-21 09:14:56 +0100

    Test your skills:Object Basics task 4

---
+++ objectBasics-JS/task4/main.js
@@ -0,0 +1,14 @@
+function Cat(name, breed, color) {
+    this.name = name;
+    this.breed = breed;
+    this.color = color;
+    this.greeting = function() {
+      console.log(`Hello, said ${ this.name } the ${ this.breed }.`);
+    }
+  }
+  
+  const cat = new Cat('Bertie', 'Cymric', 'white');
+  const cat2 = new Cat('Elfie', 'Aphrodite Giant', 'ginger');
+  
+  cat.greeting();
+  cat2.greeting();
