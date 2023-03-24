88c8a483427c598581f3b5b5a7b67d1171fcc708
Jaime Díez <jaimecuentacod@gmail.com>
2023-03-24 11:00:38 +0100

    Crear función task2 ejercicios Jesús

--- ejercicios-jesus/task2/task2.js
+++ ejercicios-jesus/task2/task2.js
@@ -1,10 +1,13 @@
 const primerArray = [2, 3, 3, 8, 9, 10, 25, 57, 82];
 const segundoArray = [5, 18, 25, 27, 45, 56, 87];
 
-const arrayJunto = [].concat(primerArray, segundoArray).sort(function(a, b){return a - b});
+function juntarArrays(arr1, arr2){
+    const juntar = [].concat(arr1, arr2).sort(function(a, b){return a - b});
+    return juntar;
+}
 
 //Pintar en HTML
 const section = document.querySelector('section');
 const para1 = document.createElement('p');
-para1.textContent = `Los arrays juntos y ordenados: ${arrayJunto}`;
+para1.textContent = `Los arrays juntos y ordenados: ${juntarArrays(primerArray, segundoArray)}`;
 section.appendChild(para1);
